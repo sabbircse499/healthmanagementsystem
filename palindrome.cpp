caea57cccbaa31f84730fbd3c459fc318071247f
Md Sabbir Hossen <122869039+sabbircse499@users.noreply.github.com>
2023-09-15 00:40:15 +0600

    Two some
    It's very easy

---
+++ palindrome.cpp
@@ -0,0 +1,4 @@
+//palindrome 
+class Solution { public: bool isPalindrome(int x) { if(x < 0) return false; long long y = 0, t = x; while (t != 0) { 	 y = y * 10 + t % 10; 	 t /= 10; 	 } 	 return x == y; } }; 
+
+
